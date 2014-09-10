86b10c1f2654c6960bacb48f15a1510dcf84a5cd
Wil A <andradewilker@gmail.com>
2014-09-10 07:47:18 -0700

    Added button to settings page and a function that handles button clicks

--- app/www/modules/settings/controllers.js
+++ app/www/modules/settings/controllers.js
@@ -4,4 +4,8 @@ angular.module('settings', ['settings.services'])
   $scope.logoutUser = function() {
     Auth.logoutUser();
   };
+
+  $scope.loadPage = function(pageName){
+    console.log("Changing to page: " +pageName );
+  };
 });
