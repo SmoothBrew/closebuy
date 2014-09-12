fdace7c93566e7b3e65b7814407dbb701fe63c88
Wil A <andradewilker@gmail.com>
2014-09-11 20:52:37 -0700

    (enhancement) Added services to bidlist and injected bidlist controller into app

---
+++ app/www/modules/bid-list/services.js
@@ -0,0 +1,13 @@
+angular.module('bidlist.services', [])
+
+.factory('BidList',  function($q) { 
+
+    var instance = {};
+
+    instance.getMyBidList = function() {
+    }
+
+    return instance;
+
+
+});
